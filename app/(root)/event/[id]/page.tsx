cea567b361eba738bf867461fa97909f96be8939
colewendling <cole@wendling.io>
2024-02-06 16:40:24 -0600

    add EVENT_BY_ID query,  create event details page

---
+++ app/(root)/event/[id]/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Page = async ({params}: {params: Promise<{id: string}>}) => {
+  const id = (await params).id;
+
+  return (
+   <>
+   <h1 className='text-3xl'>This is a Event Number: {id}</h1>
+   </>
+  );
+};
+
+export default Page;
--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -18,3 +18,18 @@ export const EVENTS_QUERY = defineQuery(
     image
 }`,
 );
+
+export const EVENT_BY_ID_QUERY = defineQuery(`*[_type == 'event' && _id == $id][0]{
+    _id, 
+    title, 
+    slug,
+    _createdAt,
+    author -> {
+      _id, name, image, bio
+    }, 
+    views, 
+    description,
+    category,
+    image,
+    pitch
+}`);
