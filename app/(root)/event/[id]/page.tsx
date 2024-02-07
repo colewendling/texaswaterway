621b89038bcc6fcd88bb9ebed595f440bdf9416a
colewendling <cole@wendling.io>
2024-02-07 11:26:30 -0600

    update event details page, add latest canary version of next

--- app/(root)/event/[id]/page.tsx
+++ app/(root)/event/[id]/page.tsx
@@ -1,3 +1,6 @@
+import { client } from '@/sanity/lib/client';
+import { EVENT_BY_ID_QUERY } from '@/sanity/lib/queries';
+import { notFound } from 'next/navigation';
 import React from 'react';
 
 export const experimental_ppr = true;
@@ -5,9 +8,13 @@ export const experimental_ppr = true;
 const Page = async ({params}: {params: Promise<{id: string}>}) => {
   const id = (await params).id;
 
+  const post = await client.fetch(EVENT_BY_ID_QUERY, {id});
+
+  if (!post) return notFound();
+
   return (
    <>
-   <h1 className='text-3xl'>This is a Event Number: {id}</h1>
+   <h1 className='text-3xl'>{post.title}</h1>
    </>
   );
 };
