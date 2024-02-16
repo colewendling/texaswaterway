7c34ecde666264d2b961fe8e986b08a8416a68d9
colewendling <cole@wendling.io>
2024-02-16 16:29:07 -0600

    add nextjs unstable_after in view.tsx

--- components/View.tsx
+++ components/View.tsx
@@ -2,16 +2,17 @@ import { client } from '@/sanity/lib/client';
 import Ping from './Ping';
 import { EVENT_VIEWS_QUERY } from '@/sanity/lib/queries';
 import { writeClient } from '@/sanity/lib/write-client';
+import { unstable_after as after } from 'next/server';
 
 const View = async ({ id }: { id: string }) => {
   const { views: totalViews } = await client
     .withConfig({ useCdn: false })
     .fetch(EVENT_VIEWS_QUERY, { id });
 
-  await writeClient
+  after(async () => await writeClient
     .patch(id)
     .set({ views: totalViews + 1 })
-    .commit();
+    .commit());
 
   return (
     <div className="view-container">
