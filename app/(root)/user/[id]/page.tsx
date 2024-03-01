0efbde101441e031a8361465b315261015c8bda6
colewendling <cole@wendling.io>
2024-03-01 11:43:00 -0600

    enable ppr on user profile page

--- app/(root)/user/[id]/page.tsx
+++ app/(root)/user/[id]/page.tsx
@@ -6,6 +6,8 @@ import { AUTHOR_BY_ID_QUERY } from '@/sanity/lib/queries';
 import { notFound } from 'next/navigation';
 import Image from 'next/image';
 
+export const experimental_ppr = true;
+
 const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const id = (await params).id;
   const session = await getServerSession(authOptions);
