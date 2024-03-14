dcb757aca19dc6a00af570e877ef370f8a888e1d
colewendling <cole@wendling.io>
2024-03-14 15:54:45 -0500

    add playlist to sanity content structure

--- sanity/structure.ts
+++ sanity/structure.ts
@@ -7,4 +7,5 @@ export const structure: StructureResolver = (S) =>
     .items([
       S.documentTypeListItem('author').title('Authors'),
       S.documentTypeListItem('event').title('Events'),
+      S.documentTypeListItem('playlist').title('Playlists'),
     ]);
