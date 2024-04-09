a1623453f49370eeada9eb49633444fd147509b6
colewendling <cole@wendling.io>
2024-04-09 17:54:31 -0500

    disable sentry in development

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,15 +1,8 @@
-// This file configures the initialization of Sentry on the server.
-// The config you add here will be used whenever the server handles a request.
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
-
-import * as Sentry from "@sentry/nextjs";
-
-Sentry.init({
-  dsn: "https://c67c660fd1cedfaebd32fd0456483b50@o4508331631312896.ingest.us.sentry.io/4508331638325248",
-
-  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: 1,
-
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
-});
+if (process.env.NODE_ENV !== 'development') {
+  import('@sentry/nextjs').then((Sentry) => {
+    Sentry.init({
+      dsn: process.env.SENTRY_DSN,
+      tracesSampleRate: 1.0,
+    });
+  });
+}
