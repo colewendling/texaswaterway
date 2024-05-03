99df126486637650f2d9f6193b1b82444e08fd54
colewendling <cole@wendling.io>
2024-05-03 10:46:30 -0500

    rm. unused useEffect in Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState, useEffect } from 'react';
+import React, { useState} from 'react';
 import { useSession, signIn, signOut } from 'next-auth/react';
 import Link from 'next/link';
 import Image from 'next/image';
