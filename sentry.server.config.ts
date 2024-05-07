3dd0101b8d891546536d7cef7231364498cecae9
colewendling <cole@wendling.io>
2024-05-07 14:20:39 -0500

    remove sentry from project

--- sentry.server.config.ts
+++
@@ -1,8 +0,0 @@
-if (process.env.NODE_ENV !== 'development') {
-  import('@sentry/nextjs').then((Sentry) => {
-    Sentry.init({
-      dsn: process.env.SENTRY_DSN,
-      tracesSampleRate: 1.0,
-    });
-  });
-}
