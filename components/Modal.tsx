5bc8aee83f64913d868c30d16d17a63649fd9c2e
colewendling <cole@wendling.io>
2024-05-30 08:29:29 -0500

    make modal more dynamic, refactor tailwind classes

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,5 +1,6 @@
 'use client';
-import React from 'react';
+
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
 const Modal = ({
@@ -11,31 +12,30 @@ const Modal = ({
   onClose: () => void;
   children: React.ReactNode;
 }) => {
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = 'hidden'; // Disable background scroll
+    } else {
+      document.body.style.overflow = ''; // Re-enable background scroll
+    }
+
+    return () => {
+      document.body.style.overflow = ''; // Cleanup on unmount
+    };
+  }, [isOpen]);
+
   if (!isOpen) return null;
 
   return ReactDOM.createPortal(
-    <div
-      onClick={onClose}
-      className="fixed inset-0 z-[1050] bg-black bg-opacity-50 overflow-y-auto overscroll-contain
-      flex items-center justify-center px-4"
-    >
-      <div
-        onClick={(e) => e.stopPropagation()}
-        className="relative w-full max-w-lg sm:max-w-xl lg:max-w-2xl bg-white rounded-3xl lg:rounded-xl shadow-lg p-10 mx-auto mt-10 mb-10 overflow-y-auto no-scrollbar flex flex-col justify-center max-h-[90vh]"
-        style={{
-          minHeight: '20rem',
-        }}
-      >
-        <button
-          onClick={onClose}
-          className="absolute top-2 right-2 w-6 h-6 bg-red-500 text-white rounded-full flex items-center justify-center shadow-md hover:bg-red-600"
-        >
+    <div onClick={onClose} className="modal-container">
+      <div onClick={(e) => e.stopPropagation()} className="modal">
+        <button onClick={onClose} className="modal-button-close">
           ✕
         </button>
         {children}
       </div>
     </div>,
-    document.body
+    document.body,
   );
 };
 
