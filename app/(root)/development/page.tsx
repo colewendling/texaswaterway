23adb611aab7f63e23d9aeede26cfe18169c1ae5
colewendling <cole@wendling.io>
2024-06-13 17:12:53 -0500

    rm. unused useEffect on /development

--- app/(root)/development/page.tsx
+++ app/(root)/development/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { clearDatabase, seedDatabase } from '@/app/actions/databaseActions';
 import { redirect } from 'next/navigation';
 
