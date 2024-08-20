6990a1a7ead46ef0e5aabdb741cfee6522a378ab
colewendling <cole@wendling.io>
2024-08-20 08:18:35 -0500

    add lib/data/categories.ts

---
+++ lib/data/categories.ts
@@ -0,0 +1,12 @@
+export const categories = [
+  { id: 'kayaking', name: 'Kayaking' },
+  { id: 'community', name: 'Community' },
+  { id: 'hiking', name: 'Hiking' },
+  { id: 'cleanup', name: 'Cleanup' },
+  { id: 'fishing', name: 'Fishing' },
+  { id: 'wildlife', name: 'Wildlife' },
+  { id: 'boating', name: 'Boating' },
+  { id: 'boyscouts', name: 'Boy Scouts' },
+  { id: 'party', name: 'Party' },
+  { id: 'marketplace', name: 'Marketplace' },
+];
