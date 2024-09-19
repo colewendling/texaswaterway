847a35f629f361c905db159f54b1cbbd23c63af1
Cole Wendling <cole@wendling.io>
2024-09-19 14:40:47 +0200

    add lake field to Session.user

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -7,7 +7,8 @@ declare module 'next-auth' {
       name?: string | null;
       email?: string | null;
       image?: string | null;
-      username: string; 
+      username: string;
+      lake: string;
     };
   }
   interface JWT {
